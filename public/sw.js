8b33731caf0acbe166395dd0fca187f2294ecd45
sam9128 <sam9128@users.noreply.github.com>
2026-07-03 11:45:52 +0800

    Persist navigation and in-progress study sessions

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="nihongo-stairs-v8-question-rewrite";
+const CACHE="nihongo-stairs-v9-session-persistence";
 const PERIODS=["115-07","115-08","115-09","115-10","115-11","115-12","116-01","116-02","116-03","116-04","116-05","116-06"];
 const PRELOAD=["./","./offline.html","./manifest.webmanifest","./icons/icon.svg","./日語階梯_完整教材.txt","./content/index.json",...PERIODS.map(period=>`./content/periods/${period}.json`)];
 
